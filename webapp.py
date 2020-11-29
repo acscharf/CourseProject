b97dee39731989b639fcf5e46ab38d04be11e5a2
acscharf <a-scharf@globis.co.jp>
2020-11-29 22:01:03 +0900

    updated webapp and model

--- webapp.py
+++ webapp.py
@@ -7,23 +7,50 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return """
-        <html><body>
-            <h2>Reflection analyzer</h2>
-            <form action="/result">
+        <html>
+        <head>
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+        <script>
+        $(document).ready(function(){
+             $('#useful').click(function(){
+                $('#reflection_area').val('Our company has products at each stage of the product lifecycle. As a manager, I will assign the right employees who can make the product succeed at each stage.');
+            });
+            $('#not_useful').click(function(){
+                $('#reflection_area').val('Product lifecyle was interesting, I am looking forward to using it.');
+            });
+        });
+       </script>
+       </head>
+        <body>
+            <h1>Reflection analyzer</h2>
+            
+                <h2>Sample video: Product Lifecycle</h2>
                 <p>
-                Enter your reflection:<br>
-                <textarea name='reflection' rows='10' cols='30'></textarea>
+                <iframe src='https://player.vimeo.com/video/371068936' width='640' height='360' frameborder='0' allowfullscreen=''></iframe>
                 </p>
-                <!--
                 <p>
-                <input type='radio' name='language' value='ja'>
-                <label for='ja'>Japanese</label><br>
-                <input type='radio' id='en' name='language' value='en'>
-                <label for='en'>English</label><br>
+                <h2>Write Your Reflection</h2>
+                <p>
+                How can you apply the learning from this course to your job or daily life?
                 </p>
-                -->
+                <p>
+
+                <form action='/result'>
+                <textarea id='reflection_area' name='reflection' rows='10' cols='40' placeholder='For example, reflect on any tasks or processes that might benefit from the knowledge you gained in this course.'></textarea>
+                </p>
+
                 <input type='submit' value='Submit'>
+
+                              
+
             </form>
+            <h3>Just want to give it a try?</h3>
+            <p>
+              <button id='useful'>Load Sample Useful Reflection</button>
+              </p>
+              <p>
+              <button id='not_useful'>Load Sample Not Useful Reflection</button>
+            </p>
         </body></html>
         """
 
@@ -36,17 +63,25 @@ def result():
 
     if doc.cats["USEFUL"] > doc.cats["NOT USEFUL"]:
         msg = 'Your reflection was great!'
+        percent = doc.cats["USEFUL"] - doc.cats["NOT USEFUL"]
     else:
-        msg = 'Hmm, try again?'
+        msg = 'Your reflection could be improved.'
+        percent = doc.cats["NOT USEFUL"] - doc.cats["USEFUL"]
 
     return """
         <html><body>
             <h2>{0}</h2>
-            <p><a href="/">Try again?</a><p>
+            <p>(We're {1:.0%} percent sure of that)</p>
+            <p>Useful reflections usually have the following:
+            <li>Around 20 words</li>
+            <li>Correct punctuation and capitalization</li>
+            <li>Words like "apply" and "use" rather than "understand"
+            </p>
+            <p><a href="/">Enter another reflection?</a><p>
         </body></html>
-        """.format(msg)
+        """.format(msg, percent)
 
 if __name__ == "__main__":
     plac.call(main)
 
-#app.run(host="localhost", debug=True)
+app.run(host="localhost", debug=True)
